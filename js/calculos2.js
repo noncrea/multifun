0202229203d1c4ec279b1f46774e4154fe7e2483
noncrea <in@noncrea.com>
2022-03-21 09:58:39 +0100

    try

---
+++ js/calculos2.js
@@ -0,0 +1,80 @@
+// Definir variables
+// var n = 0;
+
+// Funcion para leer numeros Reales
+function leerNumero() {
+    num = parseInt(
+        prompt("Porfi, escribe un número")
+    );
+    return num;
+}
+
+function mostrarMultiplo(n, m) {
+    document.getElementById('td11').innerHTML = n;
+    document.getElementById('td12').innerHTML = m;
+    document.getElementById('td13').innerHTML = multiplosDeHasta(n, m);
+}
+
+
+function mostrarFactorial( n ) {
+    document.getElementById('td21').innerHTML = n;
+    document.getElementById('td22').innerHTML = factorial( n );
+}
+
+
+function mostrarResultados( op, n, m = 0 ) {
+
+    switch (op) {
+        case 'multiplo':
+            document.getElementById('td11').innerHTML = n;
+            document.getElementById('td12').innerHTML = m;
+            document.getElementById('td13').innerHTML = multiplosDeHasta(n, m);
+        break;
+
+        case 'factorial' :
+            document.getElementById('td21').innerHTML = n;
+            document.getElementById('td22').innerHTML = factorial( n );
+        break;
+        default:
+    }
+}
+
+
+
+function multiplosDeHasta(n, m) {
+    mensaje = "Los múltiplo de " +
+        n +
+        " hasta " + m +
+        " son: ";
+
+    multiplo = 0;
+
+    // for (i = 1; multiplo < m; i++) {
+    //     multiplo = n * i;
+    //     mensaje = mensaje + multiplo + " ";
+    // }
+
+    // Inicialización de la variable contador
+    i = 1;
+
+    // Condición
+    while ( multiplo < m ) {
+        multiplo = n * i;
+        mensaje = mensaje + multiplo + " ";
+
+        // Incremento
+        i++;
+    }
+
+
+
+    return mensaje;
+}
+
+function factorial( n ) {
+    var factorial = 1;
+    for ( i=1; i <= n; i++ ) {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
